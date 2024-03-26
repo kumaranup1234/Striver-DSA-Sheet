d3c446a3b916f32c9727b391726f32e13466cd58
kumaranup1234 <anupg2551@gmail.com>
2024-03-26 11:48:41 +0530

    Questions

---
+++ src/Array/firstMissingPositive.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.HashSet;
+
+public class firstMissingPositive {
+    // Time Complexity O(N)
+    // Space Complexity O(N)
+    // required space in the question is O(1)
+    public static int firstMissingPositiveSol(int[] nums) {
+        int max = nums[0];
+        HashSet<Integer> hash = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0){
+                hash.add(nums[i]);
+            }
+            if (nums[i] > max){
+                max = nums[i];
+            }
+        }
+        // edge case
+        if(max < 0){
+            return 1;
+        }
+        for (int i = 1; i <= max; i++) {
+             if (!hash.contains(i))
+                 return i;
+        }
+        return max+1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,4,-1,1};
+        System.out.println(firstMissingPositiveSol(nums));
+    }
+}
