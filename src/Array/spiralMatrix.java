fd593b98ef09ec79b4e74a2d65b5a5091a224d0b
kumaranup1234 <anupg2551@gmail.com>
2024-03-28 17:35:17 +0530

    Questions

---
+++ src/Array/spiralMatrix.java
@@ -0,0 +1,53 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*    left         right
+ top - 11  21  31  41       right -> bottom -> left -> top
+       51  61  71  81
+       91  10  11  12
+bottom 13  14  15  16
+ */
+
+public class spiralMatrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (left <= right && top <= bottom){
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}};
+        System.out.println(spiralOrder(matrix));
+    }
+}
