90394daade6948388caf19287f7cafbb63134246
kumaranup1234 <anupg2551@gmail.com>
2024-03-31 21:39:48 +0530

    Questions

---
+++ src/Array/countSubArrayWithFixedBounds.java
@@ -0,0 +1,35 @@
+package Array;
+
+public class countSubArrayWithFixedBounds {
+
+    // Copied Solution
+    // It's a real Hard question on leetcode
+    // not able to understand to its fullest
+    // Submitted for the sake of streak
+
+    public static long countSubarrays(int[] nums, int minK, int maxK) {
+        long res = 0;
+        boolean minFound = false, maxFound = false;
+        int start = 0, minStart = 0, maxStart = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num < minK || num > maxK) {
+                minFound = false;
+                maxFound = false;
+                start = i+1;
+            }
+            if (num == minK) {
+                minFound = true;
+                minStart = i;
+            }
+            if (num == maxK) {
+                maxFound = true;
+                maxStart = i;
+            }
+            if (minFound && maxFound) {
+                res += (Math.min(minStart, maxStart) - start + 1);
+            }
+        }
+        return res;
+    }
+}
