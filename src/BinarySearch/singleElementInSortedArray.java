a4971287f53b1a102965549cfc9092c7e586eb69
kumaranup1234 <anupg2551@gmail.com>
2024-04-17 17:13:07 +0530

    Questions

--- src/BinarySearch/singleElementInSortedArray.java
+++ src/BinarySearch/singleElementInSortedArray.java
@@ -21,6 +21,19 @@ public class singleElementInSortedArray {
     // Time Complexity O(logN)
     // https://leetcode.com/problems/single-element-in-a-sorted-array/solutions/3212178/day-52-binary-search-easiest-beginner-friendly-sol/
 
+    // So, in the binary search approach
+    // If nums[mid] is equal to nums[mid + 1], it means that all pairs up to
+    // the index mid are complete, and the single non-duplicate number is
+    // somewhere to the right. So, we update start to mid + 2.
+
+    // If nums[mid] is not equal to nums[mid + 1], it means that the pair
+    // for the number at mid is incomplete, and hence, the single
+    // non-duplicate number must be somewhere to the left. So, we update end to mid.
+
+    // this is the reason why we are keeping mid even
+    // By ensuring that mid is always even, we’re always
+    // comparing with the first element of a pair.
+
     public static int singleNonDuplicate2(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
