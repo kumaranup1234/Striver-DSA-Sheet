f6c042b05d81951cff34d22d68c2a528b8bc96c4
kumaranup1234 <anupg2551@gmail.com>
2024-04-23 22:30:44 +0530

    Questions

---
+++ src/BinarySearch/searchInSortedRotatedArray2.java
@@ -0,0 +1,54 @@
+package BinarySearch;
+
+// Time Complexity O(logN) for most of the cases but if there are more duplicates it can be O(N/2)
+
+public class searchInSortedRotatedArray2 {
+    public static boolean search(int[] arr, int k) {
+        int n = arr.length; // size of the array.
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if mid points the target
+            if (arr[mid] == k) return true;
+
+            // basically we are removing duplicates else the logic is same remove duplicates
+            // then do the binary search on the sorted side
+            // either left or right
+            // if any point we encounter mid as my target we return
+
+            //Edge case:
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= k && k <= arr[mid]) {
+                    //element exists:
+                    high = mid - 1;
+                } else {
+                    //element does not exist:
+                    low = mid + 1;
+                }
+            } else { //if right part is sorted:
+                if (arr[mid] <= k && k <= arr[high]) {
+                    //element exists:
+                    low = mid + 1;
+                } else {
+                    //element does not exist:
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,0,0,1,2};
+        int target = 0;
+        System.out.println(search(nums, target));
+    }
+}
